2256d282ca187c396a0fd6326b954729a6f6656f
naiiiden <na4ko359@gmail.com>
2024-05-30 14:14:31 +0300

    newnote hook & form submit func setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react"
+
 function App() {
+  const [addNewNote, setAddNewNote] = useState({
+    "title": "",
+    "body": "",
+  });
+
+  console.log(addNewNote);
+
+  const handleNewNoteSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <main>
       <h1>Note taking application with local storage</h1>
-      <form>
+      <form onSubmit={handleNewNoteSubmit}>
         <label htmlFor="note-title">
           <span>Note title:</span>
-          <input id="note-title" type="text" placeholder="note title"/>
+          <input onInput={(e) => setAddNewNote({...addNewNote, title: e.target.value})} id="note-title" type="text" placeholder="note title"/>
         </label>
         <label htmlFor="note-body">
           <span>Note body:</span>
-          <textarea id="note-body" type="text" placeholder="note body"></textarea>
+          <textarea onInput={(e) => setAddNewNote({...addNewNote, body: e.target.value})} id="note-body" type="text" placeholder="note body"></textarea>
         </label>
         <input id="note-submit" type="submit" value="add note"/>
       </form>
