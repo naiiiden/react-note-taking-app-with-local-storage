5133299c9fc79970e5766c3be6d261b4f355aecf
naiiiden <na4ko359@gmail.com>
2024-05-30 14:00:14 +0300

    form markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 function App() {
 
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <main>
+      <h1>Note taking application with local storage</h1>
+      <form>
+        <label htmlFor="note-title">
+          <span>Note title:</span>
+          <input id="note-title" type="text" placeholder="note title"/>
+        </label>
+        <label htmlFor="note-body">
+          <span>Note body:</span>
+          <textarea id="note-body" type="text" placeholder="note body"></textarea>
+        </label>
+        <input id="note-submit" type="submit" value="add note"/>
+      </form>
+    </main>
   )
 }
 
