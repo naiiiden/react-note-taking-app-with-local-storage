e669d06f178e5b60bbaf0626136fdea5081d6f00
naiiiden <na4ko359@gmail.com>
2024-05-31 14:55:04 +0300

    singlenote add content

--- src/pages/SingleNote.jsx
+++ src/pages/SingleNote.jsx
@@ -4,19 +4,19 @@ import { NotesContext } from "../context/NotesContext"
 
 const SingleNote = () => {
     const { notes } = useContext(NotesContext);
-
     const { noteId } = useParams();
 
-    console.log("noteId", noteId);
-
-    console.log('notes from singlenote page', notes);
+    // console.log("noteId", noteId);
+    // console.log('notes from singlenote page', notes);
 
     const currentBlog = notes.find((blog) => blog.id === noteId);
-
     console.log("currentBlog", currentBlog);
 
     return (
-        <div>SingleNote</div>
+        <main>
+            <p>title: {currentBlog.title}</p>
+            <p>body: {currentBlog.body}</p>
+        </main>
     )
 }
 
