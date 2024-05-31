65ca673cfafebd04f016756899bbf17aa46ea9df
naiiiden <na4ko359@gmail.com>
2024-05-31 23:36:39 +0300

    delete note

--- src/pages/SingleNote.jsx
+++ src/pages/SingleNote.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { NotesContext } from "../context/NotesContext"
 
 const SingleNote = () => {
-    const { notes } = useContext(NotesContext);
+    const { notes, setNotes } = useContext(NotesContext);
     const { noteId } = useParams();
+    const navigate = useNavigate();
 
     // console.log("noteId", noteId);
     // console.log('notes from singlenote page', notes);
@@ -12,10 +13,16 @@ const SingleNote = () => {
     const currentBlog = notes.find((blog) => blog.id === noteId);
     console.log("currentBlog", currentBlog);
 
+    const deleteNote = () => {
+        setNotes(notes.filter((item) => item.id !== noteId));
+        navigate("/");
+    }
+
     return (
         <main>
             <p>title: {currentBlog.title}</p>
             <p>body: {currentBlog.body}</p>
+            <button onClick={deleteNote}>delete note</button>
         </main>
     )
 }
