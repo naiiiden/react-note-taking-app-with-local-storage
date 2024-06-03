89c30cd544e0f5d1fd39aecac2237943cce5bf85
naiiiden <na4ko359@gmail.com>
2024-06-03 16:23:27 +0300

    browserrouter add basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="react-note-taking-app-with-local-storage">
       <App />
     </BrowserRouter>
   </React.StrictMode>
