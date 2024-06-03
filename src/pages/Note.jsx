4eab564dd0b0f98b40a62539b32196f47a6dc298
naiiiden <na4ko359@gmail.com>
2024-06-03 08:35:13 +0300

    close note anim

--- src/pages/Note.jsx
+++ src/pages/Note.jsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Note = () => {
-  const navigate = useNavigate();
   // https://dev.to/devmdmamun/create-contextual-modal-navigation-with-react-router-v6-28k2
+  const navigate = useNavigate();
+  const [triggerCloseAnim, setTriggerCloseAnim] = useState(false);
   
   return (
-    <div className="note-modal h-40 w-40 fixed top-1/2 left-1/2 bg-red-500">
+    <div className={`${triggerCloseAnim ? "note-modal-close" : ""} note-modal h-40 w-40 fixed top-1/2 left-1/2 bg-red-500`}>
       <h3>Note</h3>
-      <button onClick={() => navigate(-1)}>XXX</button>
+      <button onClick={() => (setTriggerCloseAnim(true), setTimeout(() => navigate(-1), 200))}>XXX</button>
     </div>
   );
 };
