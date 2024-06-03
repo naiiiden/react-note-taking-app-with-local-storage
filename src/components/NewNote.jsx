894a3fd5a54f28086cf70fc70e9700f29f53cc82
naiiiden <na4ko359@gmail.com>
2024-06-03 15:49:46 +0300

    newnote add inert

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -23,6 +23,7 @@ const NewNote = () => {
       <button onClick={() => setOpenNewNoteForm(!openNewNoteForm)} className="fixed bottom-4 right-0 left-0 mx-auto text-5xl font-light shadow-2xl bg-red-600 rounded-full w-16 h-16 grid place-content-center" aria-label="Add a new note">+</button>
       <NoteForm
         closeBtnOnClick={() => setOpenNewNoteForm(!openNewNoteForm)}
+        inert={!openNewNoteForm}
         className={`${openNewNoteForm ? 'scale-y-100 scale-x-100' : 'scale-y-0 scale-x-0'} w-11/12 max-w-xl z-20 bg-gray-800 shadow-2xl rounded-lg transition-all origin-bottom fixed bottom-40 left-0 right-0 mx-auto`}
         handleFormSubmit={handleNewNoteSubmit}
         formTitle={"Add a new note"}
